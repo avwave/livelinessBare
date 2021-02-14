22eb13b3411b8ecc81b50f610e878d497927364f
Ray Arvin Rimorin <rayarvin@gmail.com>
2021-02-14 15:36:28 +0800

    so far import rncamera with vision

--- ios/livelinessBare/AppDelegate.m
+++ ios/livelinessBare/AppDelegate.m
@@ -12,7 +12,7 @@
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
 
-// #import <Firebase.h>
+#import <Firebase.h>
 
 
 static void InitializeFlipper(UIApplication *application) {
@@ -34,7 +34,7 @@ static void InitializeFlipper(UIApplication *application) {
   InitializeFlipper(application);
 #endif
 
-  // [FIRApp configure];
+  [FIRApp configure];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"livelinessBare"
